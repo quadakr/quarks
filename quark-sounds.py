47e5327995991e9b2b0b106d49f711e8baec7d36
quadakr <quadakrr@gmail.com>
2026-02-01 19:16:41 -0300

    fixes for better installation and exprience

--- quark-sounds.py
+++ quark-sounds.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
+
 import argparse
 import json
-import os
 import readline
 import select
 import subprocess
@@ -202,7 +203,7 @@ def main():
         sound_alpha = 0
 
         continue_program = input(
-            f"Disclaimer: This program is in early developement stage, theoretically, if something is wrong with libinput, it can show unexpected behavior. Sure want to continue?[y/n]: "
+            f"Disclaimer: This program is still in developement, theoretically, if something is wrong with libinput, it can show unexpected behavior. Sure want to continue?[y/n]: "
         )
 
         if continue_program != "y":
